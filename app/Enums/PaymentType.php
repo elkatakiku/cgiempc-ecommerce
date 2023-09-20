889d0e882f4dee7bbd317378297928127d71fc09
elkatakiku <79001808+elkatakiku@users.noreply.github.com>
2023-09-20 23:56:06 +0800

    Make PaymentType Enum

--- app/Enums/PaymentType.php
+++ app/Enums/PaymentType.php
@@ -2,16 +2,36 @@
 
 namespace App\Enums;
 
-enum PaymentType: string
+use Illuminate\Support\Collection;
+
+enum TypeOfPayment: string
 {
     case FULL = 'full';
     case INSTALLMENT = 'installment';
 
+    public function isFullPayment(): bool
+    {
+        return $this == self::FULL;
+    }
+
+    public function isInstallmentPayment(): bool
+    {
+        return $this == self::INSTALLMENT;
+    }
+
     public function getText(): string
     {
         return match ($this) {
-            self::FULL => 'Full Payment',
-            self::INSTALLMENT => 'Installment Payment',
+            self::FULL => "Full Payment",
+            self::INSTALLMENT => "Installment Payment",
         };
+    }
+
+    public function getTypes(): Collection
+    {
+        return collect([
+            self::FULL->value => self::FULL->getText(),
+            self::INSTALLMENT->value => self::INSTALLMENT->getText(),
+        ]);
     }
 }
